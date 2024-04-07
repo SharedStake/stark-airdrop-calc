ad82a6c62c8cd5cffd6b99f4bd74b830ca690be7
Chimera <81845287+chimera-defi@users.noreply.github.com>
2024-04-07 15:48:05 -0400

    Update starkDuneQuery.sql
    change dune query based on user feedback. based on old query

--- starkDuneQuery.sql
+++ starkDuneQuery.sql
@@ -1,56 +1,55 @@
---Possibly relevant tables based on your question: ['erc20_ethereum.evt_transfer', 'erc20_ethereum.ERC20_evt_Transfer', 'erc20_ethereum.ERC20PresetMinterPauser_evt_Transfer', 'erc20_ethereum.evt_Transfer']
-WITH transfers AS (
+SELECT veth2.address, veth2.balance AS veth2_bal
+FROM (
+  WITH transfers AS (
+  /* we count all users who minted veth2 withot discounting ALL sends to include users staking in contracts */
+  /* we only subtract sends for users who sent to the 0 address and exited via a burn */
   SELECT
-    "to" AS address,
-    SUM(value) AS tokens
+  "from",
+    NULL AS "to",
+    -SUM(value) AS value
   FROM erc20_ethereum.evt_Transfer
   WHERE
     contract_address = 0x898BAD2774EB97cF6b94605677F43b41871410B1
-    AND evt_block_number <= 15537393
-    AND (
-        "from" = 0x0000000000000000000000000000000000000000
-        OR "from" = NULL
-        OR "from" = 0x000000000000000000000000000000000000dEaD
-        OR "from" = 0xe37E2a01feA778BC1717d72Bd9f018B6A6B241D5
-        OR "from" = 0xdec2157831D6ABC3Ec328291119cc91B337272b5
-        OR "from" = 0x16BEa2e63aDAdE5984298D53A4d4d9c09e278192
-    ) AND NOT (
-        "to" = 0xe37E2a01feA778BC1717d72Bd9f018B6A6B241D5
-        OR "to" = 0xdec2157831D6ABC3Ec328291119cc91B337272b5
-        OR "to" = 0x16BEa2e63aDAdE5984298D53A4d4d9c09e278192
-    )
+    AND evt_block_number <= 15537393 -- https://www.investopedia.com/ethereum-completes-the-merge-6666337#:~:text=Key%20Takeaways,1%3A42%3A42%20EST.
   GROUP BY
-    "to"
+    "from"
   UNION ALL
+  
   SELECT
-    "from" AS address,
-    -SUM(value) AS tokens
+    NULL AS "from",
+    "to",
+    SUM(value) AS value
   FROM erc20_ethereum.evt_Transfer
   WHERE
     contract_address = 0x898BAD2774EB97cF6b94605677F43b41871410B1
     AND evt_block_number <= 15537393
-    AND (
+    AND NOT (
         "to" = 0x0000000000000000000000000000000000000000
-        OR "to" = NULL
         OR "to" = 0x000000000000000000000000000000000000dEaD
         OR "to" = 0xe37E2a01feA778BC1717d72Bd9f018B6A6B241D5
         OR "to" = 0xdec2157831D6ABC3Ec328291119cc91B337272b5
         OR "to" = 0x16BEa2e63aDAdE5984298D53A4d4d9c09e278192
+        OR "to" = 0xa919d7a5fb7ad4ab6f2aae82b6f39d181a027d35 --staking pod
+        or "to" = 0x2b228842b97ab8a1f3dcd216ec5d553ada957915 -- rewards
     )
   GROUP BY
-    "from"
+    "to"
 ), balances AS (
   SELECT
-    address,
-    SUM(tokens) AS balance
+    COALESCE("from", "to") AS address,
+    SUM(value) AS balance
   FROM transfers
   GROUP BY
-    address
-  HAVING
-    SUM(tokens) > 1000000000000000000
+    COALESCE("from", "to")
 )
 SELECT
-address, balance
-  --sum(balance)
-FROM balances
-ORDER BY balance desc
+  address,
+  balance
+FROM balances where balance > 100000000000000000
+) AS veth2
+--Possibly relevant tables based on your question: ['tokens.balances', 'erc20_ethereum.evt_Transfer', 'erc20.evt_Transfer', 'erc20_ethereum.ERC20_evt_Transfer']
+WHERE
+  veth2.balance > 0
+ORDER BY
+  veth2.balance DESC,
+  veth2.address
