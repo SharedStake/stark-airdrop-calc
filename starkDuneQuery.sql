58a165fbcacb9f74b4caa07053af1c148d3ce181
Chimera <81845287+chimera-defi@users.noreply.github.com>
2024-04-07 16:14:54 -0400

    Update starkDuneQuery.sql

--- starkDuneQuery.sql
+++ starkDuneQuery.sql
@@ -11,6 +11,11 @@ FROM (
   WHERE
     contract_address = 0x898BAD2774EB97cF6b94605677F43b41871410B1
     AND evt_block_number <= 15537393 -- https://www.investopedia.com/ethereum-completes-the-merge-6666337#:~:text=Key%20Takeaways,1%3A42%3A42%20EST.
+    AND NOT (
+        "to" = 0xe37E2a01feA778BC1717d72Bd9f018B6A6B241D5 -- saddle lp
+        OR "to" = 0xa919d7a5fb7ad4ab6f2aae82b6f39d181a027d35 --staking pod
+        or "to" = 0x2b228842b97ab8a1f3dcd216ec5d553ada957915 -- rewards
+    )
   GROUP BY
     "from"
   UNION ALL
@@ -26,11 +31,15 @@ FROM (
     AND NOT (
         "to" = 0x0000000000000000000000000000000000000000
         OR "to" = 0x000000000000000000000000000000000000dEaD
-        OR "to" = 0xe37E2a01feA778BC1717d72Bd9f018B6A6B241D5
-        OR "to" = 0xdec2157831D6ABC3Ec328291119cc91B337272b5
-        OR "to" = 0x16BEa2e63aDAdE5984298D53A4d4d9c09e278192
+        OR "to" = 0xe37E2a01feA778BC1717d72Bd9f018B6A6B241D5 -- saddle lp
+        OR "to" = 0xdec2157831D6ABC3Ec328291119cc91B337272b5 -- saddle swap 
+        OR "to" = 0x16BEa2e63aDAdE5984298D53A4d4d9c09e278192 -- snowswap 
         OR "to" = 0xa919d7a5fb7ad4ab6f2aae82b6f39d181a027d35 --staking pod
         or "to" = 0x2b228842b97ab8a1f3dcd216ec5d553ada957915 -- rewards
+    ) AND NOT (
+        "from" = 0xe37E2a01feA778BC1717d72Bd9f018B6A6B241D5 -- saddle lp
+        OR "from" = 0xa919d7a5fb7ad4ab6f2aae82b6f39d181a027d35 --staking pod
+        or "from" = 0x2b228842b97ab8a1f3dcd216ec5d553ada957915 -- rewards
     )
   GROUP BY
     "to"
